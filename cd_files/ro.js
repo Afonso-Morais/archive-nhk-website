95f0a2a43ec3d97bee5abc8b7789c479097eb5ce
Afonso-Morais <moraisafonso@protonmail.com>
2023-11-09 03:59:06 +0000

    First commit

---
+++ cd_files/ro.js
@@ -0,0 +1,33 @@
+{
+
+<!--
+// プリロードイメージ 
+IMGm01  = new Image(); IMGm01.src  = "images/botan_news.gif";
+IMGm01_ = new Image(); IMGm01_.src = "images/botan_news_r.gif";
+IMGm02  = new Image(); IMGm02.src  = "images/botan_sakuhin.gif";
+IMGm02_ = new Image(); IMGm02_.src = "images/botan_sakuhin_r.gif";
+IMGm03  = new Image(); IMGm03.src  = "images/botan_charactor.gif";
+IMGm03_ = new Image(); IMGm03_.src = "images/botan_charactor_r.gif";
+IMGm04  = new Image(); IMGm04.src  = "images/botan_staff.gif";
+IMGm04_ = new Image(); IMGm04_.src = "images/botan_staff_r.gif";
+IMGm05  = new Image(); IMGm05.src  = "images/botan_story.gif";
+IMGm05_ = new Image(); IMGm05_.src = "images/botan_story_r.gif";
+IMGm06  = new Image(); IMGm06.src  = "images/botan_books.gif";
+IMGm06_ = new Image(); IMGm06_.src = "images/botan_books_r.gif";
+IMGm07  = new Image(); IMGm07.src  = "images/botan_cd.gif";
+IMGm07_ = new Image(); IMGm07_.src = "images/botan_cd_r.gif";
+IMGm08  = new Image(); IMGm08.src  = "images/botan_goods.gif";
+IMGm08_ = new Image(); IMGm08_.src = "images/botan_goods_r.gif";
+IMGm09  = new Image(); IMGm09.src  = "images/botan_event.gif";
+IMGm09_ = new Image(); IMGm09_.src = "images/botan_event_r.gif";
+IMGm10  = new Image(); IMGm10.src  = "images/botan_secret.gif";
+IMGm10_ = new Image(); IMGm10_.src = "images/botan_secret_r.gif";
+IMGm11  = new Image(); IMGm11.src  = "images/botan_voice.gif";
+IMGm11_ = new Image(); IMGm11_.src = "images/botan_voice_r.gif";
+
+
+// -- ロールオーバー
+function ro(imgname,imgno) {
+	document.images[imgname].src = eval(imgno + ".src");
+}
+}
